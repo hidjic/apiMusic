7dc743c4780f87d9c8edf8d8d5e24bd5a6dea8e1
hidjic <stephaneboivin.hidjic@yahoo.fr>
2020-10-21 16:50:47 +0200

    test

--- src/main/java/org/ex/apiZiq/AuthorizationCode.java
+++ src/main/java/org/ex/apiZiq/AuthorizationCode.java
@@ -1,5 +1,7 @@
 package org.ex.apiZiq;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.ex.apiZiq.models.SpotifyApiSingleton;
 
 import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
@@ -7,10 +9,11 @@ import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredential
 //@Service
 public class AuthorizationCode {
 	
-//	private static final Logger log = LogManager.getLogger(AuthorizationCode.class);
+	private static final Logger log = LogManager.getLogger(AuthorizationCode.class);
 		
 	public static void authorizationCode(String code) {
 		AuthorizationCodeCredentials acc = SpotifyApiSingleton.getAuthorizationCodeCredentials(code);
+		log.info("acc.getAccessToken() => " + acc.getAccessToken());
 		SpotifyApiSingleton.getsApi().setAccessToken(acc.getAccessToken());
 		SpotifyApiSingleton.getsApi().setRefreshToken(acc.getRefreshToken());
 	}
